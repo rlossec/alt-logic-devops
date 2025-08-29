bb790b7778af71a7f858fefb597cfce06ce5e919
rlossec <raphaelossec@gmail.com>
2025-08-29 10:03:42 +0200

    Logic-Q1 init

---
+++ logique/string/length_string.py
@@ -0,0 +1,10 @@
+## 1. Fonction qui prend une chaîne de caractères en paramètre et retourne sa longueur après avoir supprimé tous les espaces.
+
+def length_string(string):
+    pass
+
+
+
+if __name__ == "__main__":
+
+    assert length_string("Bonjour le monde !") == 16
