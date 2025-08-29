9c9d085e67841fd87d2748bd6f92b992a07f713a
rlossec <raphaelossec@gmail.com>
2025-08-29 10:14:22 +0200

    Logic-Q1 : planification des tests

--- logique/string/length_string.py
+++ logique/string/length_string.py
@@ -1,9 +1,28 @@
 ## 1. Fonction qui prend une chaîne de caractères en paramètre et retourne sa longueur après avoir supprimé tous les espaces.
 
 def length_string(string: str) -> int:
+    
     string_without_space = string.replace(' ', '')
     return len(string_without_space)
 
 
 if __name__ == "__main__":
+    
     assert length_string("Bonjour le monde !") == 15
+
+    # Tests cas particuliers
+    assert length_string("") == 0
+    assert length_string(" ") == 0
+
+    # # Tests si le typage n'est pas string
+    # ## Si le type est convertible
+    # assert length_string(145) == 3
+
+    # ## Si le type est convertible
+    # assert length_string(dict()) == 0
+    # assert length_string(None) == 0
+    # assert length_string(True) == 0
+    # assert length_string(False) == 0
+    # assert length_string(1.5) == 0
+
+          
