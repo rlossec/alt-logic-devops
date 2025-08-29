545b02c112745a24ae4706ae0e2bb5caf07f9e42
rlossec <raphaelossec@gmail.com>
2025-08-29 10:07:44 +0200

    Logic-Q1 logic ok

--- logique/string/length_string.py
+++ logique/string/length_string.py
@@ -1,10 +1,9 @@
 ## 1. Fonction qui prend une chaîne de caractères en paramètre et retourne sa longueur après avoir supprimé tous les espaces.
 
-def length_string(string):
-    pass
-
+def length_string(string: str) -> int:
+    string_without_space = string.replace(' ', '')
+    return len(string_without_space)
 
 
 if __name__ == "__main__":
-
-    assert length_string("Bonjour le monde !") == 16
+    assert length_string("Bonjour le monde !") == 15
