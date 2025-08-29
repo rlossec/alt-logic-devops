0ab1589c270509eefc685dad0f216f9d393c4305
rlossec <raphaelossec@gmail.com>
2025-08-29 16:12:03 +0200

    Logic/Array-Object : Ex 1 - Documentation

---
+++ logique/array-object/1_filter_by_property.py
@@ -0,0 +1,21 @@
+# Pydantic pour le typage User
+
+
+def filter_by_property(objects_list: object, property: str, value: any) -> list:
+    pass
+
+
+if __name__ == "__main__":
+    users = [
+        {"id": 1, "name": "Alice", "age": 25, "active": True},
+        {"id": 2, "name": "Bob", "age": 30, "active": False},
+        {"id": 3, "name": "Charlie", "age": 35, "active": True}
+    ]
+    print(filter_by_property(users, 'active', True))
+    # [{"id": 1, "name": "Alice", "age": 25, "active": True}, {"id": 3, "name": "Charlie", "age": 35, "active": True}]
+    print(filter_by_property(users, 'name', "Alice"))
+    # [{"id": 1, "name": "Alice", "age": 25, "active": True}]
+
+    # Tests
+    # assert filter_by_property(users, 'active', True) == [{"id": 1, "name": "Alice", "age": 25, "active": True}, {"id": 3, "name": "Charlie", "age": 35, "active": True}]
+    # assert filter_by_property(users, 'name', "Alice") == [{"id": 1, "name": "Alice", "age": 25, "active": True}]
