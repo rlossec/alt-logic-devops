a46cdf20e9ea69c5f3c17b753599675e170081dd
rlossec <raphaelossec@gmail.com>
2025-08-29 16:15:21 +0200

    Logic/Array-Object : Ex 1 - Resolution simple, deux tests

--- logique/array-object/1_filter_by_property.py
+++ logique/array-object/1_filter_by_property.py
@@ -1,8 +1,8 @@
 # Pydantic pour le typage User
 
 
-def filter_by_property(objects_list: object, property: str, value: any) -> list:
-    pass
+def filter_by_property(objects_list: list, property: str, value: any) -> list:
+    return [object_item for object_item in objects_list if object_item[property] == value]
 
 
 if __name__ == "__main__":
@@ -17,5 +17,5 @@ if __name__ == "__main__":
     # [{"id": 1, "name": "Alice", "age": 25, "active": True}]
 
     # Tests
-    # assert filter_by_property(users, 'active', True) == [{"id": 1, "name": "Alice", "age": 25, "active": True}, {"id": 3, "name": "Charlie", "age": 35, "active": True}]
-    # assert filter_by_property(users, 'name', "Alice") == [{"id": 1, "name": "Alice", "age": 25, "active": True}]
+    assert filter_by_property(users, 'active', True) == [{"id": 1, "name": "Alice", "age": 25, "active": True}, {"id": 3, "name": "Charlie", "age": 35, "active": True}]
+    assert filter_by_property(users, 'name', "Alice") == [{"id": 1, "name": "Alice", "age": 25, "active": True}]
