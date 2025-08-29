cf2eeb3bade690bc84e4998697c0e883ddaef1e3
rlossec <raphaelossec@gmail.com>
2025-08-29 10:45:59 +0200

    Logic-Q2 : Test for empty object and basic types

--- logique/object/get_object_values.py
+++ logique/object/get_object_values.py
@@ -1,6 +1,10 @@
 
 def get_object_values(object: dict) -> list:
-    return list(object.values())
+    try:
+        values = list(object.values())
+    except AttributeError:
+        return []
+    return values
 
 
 if __name__ == "__main__":
@@ -10,4 +14,19 @@ if __name__ == "__main__":
         "level3": 95
     }
     assert get_object_values(scores) == [100, 85, 95]
+
+    # Test cas particuliers
+    ## Objets vides
+    assert get_object_values(object()) == []
+
+    # Test si l'objet est un autre type
+    assert get_object_values(True) == []
+    assert get_object_values(None) == []
+    assert get_object_values(1) == []
+    assert get_object_values("Hello") == []
+    assert get_object_values(dict()) == []
+    assert get_object_values(list()) == []
+
+
+
 
